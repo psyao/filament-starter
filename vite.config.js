b9257f1c49bef7ff575050e60f668f4bae93397d
Steve Aguet <psyaosan@gmail.com>
2022-12-19 10:21:09 +0100

    First commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+const fs = require('node:fs');
+import {defineConfig} from 'vite';
+import {homedir} from 'os'
+import {resolve} from 'path'
+import laravel from 'laravel-vite-plugin';
+
+let host = 'filament.test';
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: detectServerConfig(host),
+});
+
+
+
+function detectServerConfig(host) {
+  let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+  let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+  if (!fs.existsSync(keyPath)) {
+    return {}
+  }
+
+  if (!fs.existsSync(certificatePath)) {
+    return {}
+  }
+
+  return {
+    hmr: {host},
+    host,
+    https: {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certificatePath),
+    },
+  }
+}
